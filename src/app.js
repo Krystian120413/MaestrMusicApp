22861918b954053e700b13d6fe0d52d96785b9d8
Krystian Zbyrad <=>
2022-05-29 18:28:08 +0200

    started making backend

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+app.use(cors())
+
+
+app.get('/song', (req, res) => {
+    res.sendFile('../assets/song1.mp3', { root: __dirname });
+});
+
+
+app.listen(5000, () => {
+    console.log('Listening on port 5000!')
+});
+
