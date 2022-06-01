7cf78cd1ab8fa8b60d09299055c10142ce0d91af
Krystian Zbyrad <kz113797@stud.ur.edu.pl>
2022-06-01 02:12:36 +0200

    simple backend works, one song playing on admin panel index.html

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
-const path = require('path');
 const app = express();
-const songs = require('./src/routes/songs')
 app.use(cors())
 
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
 app.get('/song', (req, res) => {
-    res.sendFile(songs.toString(), { root: __dirname });
+    const range = req.headers.range
+    if(!range){
+        res.status(400).send("Requires Range Header")
+    }
+
+    const songPath = 'assets/Motion_Trio-Happy_Band.mp3'
+    const songSize = fs.statSync(songPath).size
+
+    const CHUNK_SIZE = 10 ** 6  // 1MB
+    const start = Number(range.replace(/\D/g, ""))
+    const end = Math.min(start + CHUNK_SIZE, songSize - 1)
+
+    const contentLength = end - start + 1
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${songSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "audio/mpeg",
+    }
+
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers)
+
+    const songStream = fs.createReadStream(songPath, { start, end })
+
+    songStream.pipe(res)
+
+
+
+    // res.sendFile('data/mockdata.js', { root: __dirname });
 });
 
 
--- src/routes/songs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const songs = require('../data/mockdata');
-
-router.get('/', (req, res) => {
-    res.json(songs);
-})
-
-module.exports = router;
