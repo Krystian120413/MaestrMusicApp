6f90f82179ab2c3879eed501f3eba90fdeda1e76
Krystian Zbyrad <Krystian12053@gmail.com>
2022-09-11 16:38:17 +0200

    wip

---
+++ src/controllers/songsController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async findOne(req, res, next){},
+
+    async findAll(req, res){},
+
+    async create(req, res){},
+
+    async update(req, res, next){},
+
+    async delete(req, res, next){}
+};
--- src/routes/songs.js
+++ src/routes/songs.js
@@ -1,7 +1,13 @@
 const Router = require('express').Router;
+const songsController = require('../controllers/songsController');
+const catchAsync = require('../middlewares/errors');
+const songs = require('../data/songs.json');
 
 module.exports = () => {
     const api = Router();
+
+    //GET songs-info/:id
+    api.get('/songs-info/:id', catchAsync(songsController.findOne));
 
     return api;
 };
