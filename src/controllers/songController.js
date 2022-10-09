16a05e834af96d269c7af5bc824e90bce9368f63
Krystian Zbyrad <krystian.zbyrad@kruko.io>
2022-10-09 16:57:21 +0200

    fixed structure a little

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -1,10 +1,10 @@
 const songs = require('./src/data/songs.json');
 
-module.exports = () => {
+module.exports = {
   // findOne(req, res, next) {},
 
   async findAll(req, res) {
     const song = await songs;
-    return res.status(200).send({ data: songs })
+    return res.status(200).send({ data: songs });
   }
 };
--- src/controllers/songsController.js
+++ src/controllers/songsController.js
@@ -1,7 +1,12 @@
+const songs = require('./src/data/songs.json');
+
 module.exports = {
     async findOne(req, res, next){},
 
-    async findAll(req, res){},
+    async findAll(req, res){
+        const allSongs = await songs;
+        return res.status(200).send({ data: allSongs });
+    },
 
     async create(req, res){},
 
